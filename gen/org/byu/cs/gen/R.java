49d75fb1227cb40dbf7fb6078eaddc15d194105c
unknown <Scott@.(none)>
2010-04-12 20:31:08 -0600

    Changed the 20 in the application name to Twenty

--- gen/org/byu/cs/gen/R.java
+++ gen/org/byu/cs/gen/R.java
@@ -20,31 +20,27 @@ public final class R {
         public static final int twentymingenlogo=0x7f020006;
     }
     public static final class id {
-        public static final int documents=0x7f070009;
-        public static final int home=0x7f07000a;
-        public static final int loggedinlabel=0x7f070004;
-        public static final int loginbutton=0x7f070002;
-        public static final int logoutbutton=0x7f070005;
-        public static final int more=0x7f07000b;
-        public static final int programlogo=0x7f070000;
-        public static final int searchbutton=0x7f070006;
-        public static final int suggestions=0x7f070007;
-        public static final int todo=0x7f070008;
-        public static final int welcomelabel=0x7f070001;
-        public static final int welcomeprogramlogo=0x7f070003;
+        public static final int documents=0x7f060009;
+        public static final int home=0x7f06000a;
+        public static final int loggedinlabel=0x7f060004;
+        public static final int loginbutton=0x7f060002;
+        public static final int logoutbutton=0x7f060005;
+        public static final int more=0x7f06000b;
+        public static final int programlogo=0x7f060000;
+        public static final int searchbutton=0x7f060006;
+        public static final int suggestions=0x7f060007;
+        public static final int todo=0x7f060008;
+        public static final int welcomelabel=0x7f060001;
+        public static final int welcomeprogramlogo=0x7f060003;
     }
     public static final class layout {
         public static final int main=0x7f030000;
         public static final int welcome=0x7f030001;
     }
     public static final class menu {
-        public static final int genmenu=0x7f060000;
-    }
-    public static final class raw {
-        public static final int certstore=0x7f040000;
+        public static final int genmenu=0x7f050000;
     }
     public static final class string {
-        public static final int app_name=0x7f050001;
-        public static final int hello=0x7f050000;
+        public static final int app_name=0x7f040000;
     }
 }
